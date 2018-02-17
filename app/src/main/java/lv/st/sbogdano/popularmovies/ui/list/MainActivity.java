17ffae0efa3a699424ec71c11ce6ef27959ddd9f
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2018-02-17 11:24:42 +0200

    Added dependencies into gradle

--- app/src/main/java/lv/st/sbogdano/popularmovies/ui/list/MainActivity.java
+++ app/src/main/java/lv/st/sbogdano/popularmovies/ui/list/MainActivity.java
@@ -1,7 +1,9 @@
-package lv.st.sbogdano.popularmovies;
+package lv.st.sbogdano.popularmovies.ui.list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import lv.st.sbogdano.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/lv/st/sbogdano/popularmovies/ui/list/MainActivityViewModel.java
@@ -0,0 +1,8 @@
+package lv.st.sbogdano.popularmovies.ui.list;
+
+/**
+ * Created by sbogdano on 17/02/2018.
+ */
+
+public class MainActivityViewModel {
+}
